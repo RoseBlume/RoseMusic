42aae2395e143839f1680a7889485aa03194291d
James <james.rose1103@protonmail.com>
2024-11-01 10:53:49 -0400

    Fix

--- scripts/inst.bat
+++ scripts/inst.bat
@@ -1,10 +1,12 @@
 @echo off
 cd ..
 cd node_modules
-for /D %%d in (*) do (
-    echo Installing in %%d
-    cd %%d
-    npm install
-    cd ..
+for /R %%d in (.) do (
+    if exist "%%d\package.json" (
+        echo Installing in %%d
+        cd %%d
+        npm install
+        cd /d %~dp0
+    )
 )
 echo Done!
